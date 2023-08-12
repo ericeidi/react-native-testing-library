cfb8a5a4894d24f46d5198d83d9aed55253b7f80
ericeidi <ericen01@gmail.com>
2023-08-12 10:21:55 -0400

    creating tests for StarRating component

--- src/components/StarRating/__tests__/StartRating.test.tsx
+++ src/components/StarRating/__tests__/StartRating.test.tsx
@@ -3,9 +3,22 @@ import {render} from '@testing-library/react-native';
 import {StarRating} from '../StarRating';
 
 describe('StartRating', () => {
-  it('should render the icon when the average value is passed', () => {
-    const {getByText} = render(<StarRating rating={{average: 2}} />);
-    const element = getByText('2');
-    expect(element).toBeTruthy();
+  describe('rating was passed', () => {
+    it('should render the text', () => {
+      const {getByText} = render(<StarRating rating={{average: 2}} />);
+      const element = getByText('2');
+      expect(element).toBeTruthy();
+    });
+    it('should render the star icon', () => {
+      const {getByTestId} = render(<StarRating rating={{average: 2}} />);
+      const icon = getByTestId('starIcon');
+      expect(icon).toBeTruthy();
+    });
+  });
+  describe('rating was not passed', () => {
+    it('should not render', () => {
+      const {root} = render(<StarRating />);
+      expect(root).toBeUndefined();
+    });
   });
 });
